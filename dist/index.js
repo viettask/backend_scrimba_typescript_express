1bc14c4953cd725916db8908a63f3cb29dd6a4b6
viettask <viet.nguyen@viettask.com>
2026-01-02 22:14:55 +1000

    Express to show data

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,4 @@
+"use strict";
 /*
 CHALLENGE1 : Build a basic Express app using vanilla JavaScript
 1. Import Express and create an app instance.
@@ -6,13 +7,15 @@ CHALLENGE1 : Build a basic Express app using vanilla JavaScript
    
 You won't need to create any endpoints or routes yet
 */
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 /*
-MINI CHALLENGE 2: Add some types  
+MINI CHALLENGE 2: Add some types
 1. Use the `Express` type where it makes sense
 2. Add a `void` return type where it makes sense
 */
-
 /*
 CHALLENGE 3: Send back a response
 1. Add a GET route for `/` that responds with an empty JSON object.
@@ -21,7 +24,6 @@ CHALLENGE 3: Send back a response
    
 (Ignore any TypeScript warning for now)
 */
-
 /*
 CHALLENGE 4: Respond with some data!
 1. Create an object called `pet` before the `/` route that includes:
@@ -33,21 +35,23 @@ CHALLENGE 4: Respond with some data!
 3. Update your existing GET `/` route to return that pet object
 4. Compile the TypeScript and run the resulting JavaScript to see it in action
 */
-
-
-import express from 'express'
-import type { Express } from 'express'
-import {pets} from './data/pets'
-
-const PORT : number = 8000
-const app : Express = express()
-
-
-app.get('/',(req,res) => {
-    res.json(pets)
-})
-
-app.listen(8000, (): void =>
-{
-    console.log("listening on port: ", PORT)
-})
+const express_1 = __importDefault(require("express"));
+const PORT = 8000;
+const app = (0, express_1.default)();
+const pets = [{
+        name: "Rubik",
+        species: "Cat",
+        adopted: true,
+        age: 3
+    }, {
+        name: "cookie",
+        species: "dog",
+        adopted: true,
+        age: 2
+    }];
+app.get('/', (req, res) => {
+    res.json(pets);
+});
+app.listen(8000, () => {
+    console.log("listening on port: ", PORT);
+});
--- note.md
+++ note.md
@@ -9,8 +9,27 @@
 check node version and install ts match with that version 
 5. npm i -D @tsconfig/node20
 
+6. Node does not know TS
+npx tsc
+ls dist
+cat dist/index.js
+node dist/index.js
+7. delete "script" "test" in package.json
+  "scripts": {
+    "build": "npx tsc"
+  }
 
-
+  to build & run the program in one go.
+  CHALLENGE
+    "0": "Create a start script that",
+    "1": "Runs the TypeScript Compiler, then",
+    "2": "Runs the compiled app in 'dist/index.js'",
+    "HINT": "You can chain commands with &&"
+    
+    "scripts": {
+    "build": "npx tsc",
+    "start": "npx tsc && node dist/index.js"
+  }
 
 
 Docker 
@@ -23,3 +42,13 @@ Docker
 5. docker start
 6. docker rm <Nama or ID>
 7. docker ps -a //List all containers (including stopped ones)
+8. docker run -it --rm --init node:22-alpine node
+    -it makes the container interactive, so we can type into it
+    -rm makes docker automatically delete container when we're done
+    -init helps clean up processes when we exit
+    node:22-alpine is the image we want
+    node tells Docker to start the Node REPL
+
+.gitignore
+git rm -r --cached node_modules/
+git check-ignore -v node_modules
